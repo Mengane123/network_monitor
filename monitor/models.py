687446e340bae13d69baece7fbbe8932c235ae56
Mengane123 <prashantmengane007@gmail.com>
2025-02-26 13:59:09 +0530

    added more models portstatus , securityAlert , anomalyDetection

--- monitor/models.py
+++ monitor/models.py
@@ -27,3 +27,56 @@ class NetworkConnection(models.Model):
         ]
 
 
+class PortStatus(models.Model):
+    port_number = models.IntegerField()
+    status = models.CharField(max_length=20) #will indicate , if the port if open , closed or filtered
+    service = models.CharField(max_length=100 , null=True , blank=True)
+    
+    last_checked = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['port_number']),
+            models.Index(fields=['status']),
+        ]
+
+
+class SecurityAlert(models.Model):
+    SEVERITY_CHOICES = [
+        ('LOW' , 'Low'),
+        ('MEDIUM','Medium'),
+        ('HIGH','High'),
+        ('CRITICAL','Critical'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    severity = models.CharField(max_length=10 , choices=SEVERITY_CHOICES)
+    source_ip = models.GenericIPAddressField(null=True , blank=True)
+    is_resolved = models.BooleanField(default=False)
+    resolution_notes = models.TextField(null=True , blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['timestamp']),
+            models.Index(fields=['severity']),
+            models.Index(fields=['is_resolved']),
+        ]
+
+
+class AnomalyDetectionModel(models.Model):
+    name = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(default=timezone.now)
+    last_trained = models.DateTimeField(default=timezone.now)
+    accuracy_score = models.FloatField(null=True , blank=True)
+    model_file = models.FileField(upload_to='ml_models/')
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['is_active']),
+        ]
+
+
+        
