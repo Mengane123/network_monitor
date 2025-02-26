21dae10acdab612501817d5495fb2f855fe27ff6
Mengane123 <prashantmengane007@gmail.com>
2025-02-26 10:45:12 +0530

    created NetworkConnection model

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone 
+
 
 # Create your models here.
+# we have to create 4 models 
+# 1 : Network Connection 
+# 2 : Port Status 
+# 3 : Security Alerts 
+# 4 : Anomaly Detection Model 
+
+
+class NetworkConnection(models.Model):
+    source_ip = models.GenericIPAddressField()
+    destination_ip = models.GenericIPAddressField()
+    source_port = models.IntegerField()
+    destination_port = models.IntegerField()
+    protocol = models.CharField(max_length=10)
+    timestamp = models.DateTimeField(default=timezone.now)
+    bytes_transferred = models.BigIntegerField(default=0)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['timestamp']),
+            models.Index(field=['source_ip']),
+            models.Index(field=['destination_ip']),
+        ]
+
+
