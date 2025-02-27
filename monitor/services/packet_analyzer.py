985d01bd43d2f9619bda5e2dc4b622254f92cdb7
Mengane123 <prashantmengane007@gmail.com>
2025-02-27 20:44:00 +0530

    script for capturing live network traffic using scapy.all

--- monitor/services/packet_analyzer.py
+++ monitor/services/packet_analyzer.py
@@ -13,10 +13,33 @@ class PacketAnalyzer:
         self.stop_flag = threading.Event() #threading.Event() object is used to control when the packet sniffing should stop 
 
     #this method is called each time a packet is captured 
-    def packet_process(self,packet):
+    def process_packet(self,packet):
         if IP in packet: #checks if the packet contains the ip layer 
             #extracting relevant information from the packet 
-            
+            connection = NetworkConnection(
+                source_ip=packet[IP].src,
+                destination_ip = packet[IP].dst,
+                source_port = packet.sport if hasattr(packet , 'sport') else 0,
+                destination_port = packet.dport if hasattr(packet , 'dport') else 0,
+                protocol = packet.proto if hasattr(packet , 'proto') else 'Unknown',
+                bytes_transferred = len(packet)
+
+            )
+            connection.save()
+
+
+    def start_capture(self):
+        sniff(prn=self.process_packet , store=0, stop_filter=lambda _: self.stop_flag.is_set())
+        #here sniff() captures network traffic , and then prn=self.process_packet calls process_packet() method for each captured packet , 
+        #store=0 prevents storing packets in memory (to save memory usage)
+        #now the lambda , stop_filter checks if the self.stop_flag is set , if it is set (means if is_set() return TRUE) , sniff() will stop capturing
+
+    def stop_filter(self):
+        self.stop_flag.set()
+
+
+
+        
 
 
 
